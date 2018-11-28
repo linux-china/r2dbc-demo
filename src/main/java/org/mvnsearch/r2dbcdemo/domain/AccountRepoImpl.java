3b6f7dd09b3bdc2b607c5655475d2be19421e685
linux_china <libing.chen@gmail.com>
2018-11-27 22:00:59 -0800

    add save

--- src/main/java/org/mvnsearch/r2dbcdemo/domain/AccountRepoImpl.java
+++ src/main/java/org/mvnsearch/r2dbcdemo/domain/AccountRepoImpl.java
@@ -4,6 +4,7 @@ import io.r2dbc.client.R2dbc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * account repository implementation
@@ -27,5 +28,12 @@ public class AccountRepoImpl implements AccountRepo {
                             return account;
                         })));
 
+    }
+
+    @Override
+    public Mono<Integer> save(Account account) {
+        return r2dbc.inTransaction(handle ->
+                handle.execute("INSERT INTO  account(nick,email,created_at, updated_at) values($1,$2,current_timestamp(),current_timestamp())", account.getNick(), account.getEmail()))
+                .reduce(0, (x1, x2) -> x1 + x2);
     }
 }
