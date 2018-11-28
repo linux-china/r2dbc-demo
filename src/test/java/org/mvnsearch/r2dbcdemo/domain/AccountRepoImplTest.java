62d8625cfcf17aca9e1dc8065d5bb21579ba8d87
linux_china <libing.chen@gmail.com>
2018-11-27 21:40:32 -0800

    account repository implementation test

---
+++ src/test/java/org/mvnsearch/r2dbcdemo/domain/AccountRepoImplTest.java
@@ -0,0 +1,24 @@
+package org.mvnsearch.r2dbcdemo.domain;
+
+import com.github.database.rider.core.api.dataset.DataSet;
+import org.junit.jupiter.api.Test;
+import org.mvnsearch.r2dbcdemo.BaseTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * account repository implementation test
+ *
+ * @author linux_china
+ */
+@DataSet("/dataset/account.xml")
+public class AccountRepoImplTest extends BaseTest {
+    @Autowired
+    private AccountRepo accountRepo;
+
+    @Test
+    public void testFindAll() {
+        accountRepo.findAll().subscribe(account -> {
+            System.out.println(account.getNick());
+        });
+    }
+}
