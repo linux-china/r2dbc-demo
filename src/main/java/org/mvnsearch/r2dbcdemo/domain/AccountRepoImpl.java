824c545f27ab77e5c79e27e4c79858aee6392205
linux_china <libing.chen@gmail.com>
2018-11-27 23:32:38 -0800

    polishing

--- src/main/java/org/mvnsearch/r2dbcdemo/domain/AccountRepoImpl.java
+++ src/main/java/org/mvnsearch/r2dbcdemo/domain/AccountRepoImpl.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Date;
+
 /**
  * account repository implementation
  *
@@ -25,6 +27,10 @@ public class AccountRepoImpl implements AccountRepo {
                             account.setId(row.get("id", Integer.class));
                             account.setEmail(row.get("email", String.class));
                             account.setNick(row.get("nick", String.class));
+                            account.setPhone(row.get("phone", String.class));
+                            account.setPassword(row.get("passwd", String.class));
+                            account.setCreatedAt(row.get("created_at", Date.class));
+                            account.setUpdatedAt(row.get("updated_at", Date.class));
                             return account;
                         })));
 
@@ -33,7 +39,7 @@ public class AccountRepoImpl implements AccountRepo {
     @Override
     public Mono<Integer> save(Account account) {
         return r2dbc.inTransaction(handle ->
-                handle.execute("INSERT INTO  account(nick,email,created_at, updated_at) values($1,$2,current_timestamp(),current_timestamp())", account.getNick(), account.getEmail()))
+                handle.execute("INSERT INTO  account(nick,email,phone,passwd,created_at, updated_at) values($1,$2,'18667135137','123456',current_timestamp(),current_timestamp())", account.getNick(), account.getEmail()))
                 .reduce(0, (x1, x2) -> x1 + x2);
     }
 }
