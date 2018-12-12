317291aef668da5fa7fc55a8cb72de963e65c442
linux_china <libing.chen@gmail.com>
2018-12-12 11:12:10 -0800

    account reactive repository

---
+++ src/main/java/org/mvnsearch/r2dbcdemo/domain/AccountReactiveRepo.java
@@ -0,0 +1,16 @@
+package org.mvnsearch.r2dbcdemo.domain;
+
+import org.springframework.data.r2dbc.repository.query.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+/**
+ * account reactive repository
+ *
+ * @author linux_china
+ */
+public interface AccountReactiveRepo extends ReactiveCrudRepository<Account, Integer> {
+
+    @Query("SELECT * FROM account WHERE nick = $1")
+    Mono<Account> findByNick(String nick);
+}
