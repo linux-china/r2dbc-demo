e9f96bc67037e76d3ab8b9b09584d9eeafe6c38d
linux_china <libing.chen@gmail.com>
2018-12-12 11:12:49 -0800

    add reactive repo

--- src/main/java/org/mvnsearch/r2dbcdemo/PortalController.java
+++ src/main/java/org/mvnsearch/r2dbcdemo/PortalController.java
@@ -1,11 +1,14 @@
 package org.mvnsearch.r2dbcdemo;
 
 import org.mvnsearch.r2dbcdemo.domain.Account;
+import org.mvnsearch.r2dbcdemo.domain.AccountReactiveRepo;
 import org.mvnsearch.r2dbcdemo.domain.AccountRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * portal controller
@@ -16,9 +19,16 @@ import reactor.core.publisher.Flux;
 public class PortalController {
     @Autowired
     private AccountRepo accountRepo;
+    @Autowired
+    AccountReactiveRepo accountReactiveRepo;
 
     @RequestMapping("/accounts")
     public Flux<Account> allAccounts() {
         return accountRepo.findAll();
+    }
+
+    @RequestMapping("/account/{nick}")
+    public Mono<Account> showAccountByNick(@PathVariable String nick) {
+        return accountReactiveRepo.findByNick(nick);
     }
 }
