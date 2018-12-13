d25e945205fb3cc616d821ffa5c6675c30c51151
linux_china <libing.chen@gmail.com>
2018-12-12 21:17:33 -0800

    add data imported

--- src/main/java/org/mvnsearch/r2dbcdemo/DataLoader.java
+++ src/main/java/org/mvnsearch/r2dbcdemo/DataLoader.java
@@ -1,26 +1,38 @@
 package org.mvnsearch.r2dbcdemo;
 
 import org.mvnsearch.r2dbcdemo.domain.Account;
-import org.mvnsearch.r2dbcdemo.domain.AccountRepo;
+import org.mvnsearch.r2dbcdemo.domain.AccountReactiveRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
 public class DataLoader implements ApplicationRunner {
 
-    private AccountRepo accountRepo;
+    private AccountReactiveRepo accountReactiveRepo;
 
     @Autowired
-    public DataLoader(AccountRepo accountRepo) {
-        this.accountRepo = accountRepo;
+    public DataLoader(AccountReactiveRepo accountRepo) {
+        this.accountReactiveRepo = accountRepo;
     }
 
     public void run(ApplicationArguments args) {
-        Account account = new Account();
-        account.setEmail("libing.chen@gmail.com");
-        account.setNick("linux_china");
-        accountRepo.save(account).subscribe();
+        accountReactiveRepo.findByNick("007").hasElement().subscribe(founded -> {
+            if (!founded) {
+                Account account = new Account();
+                account.setEmail("007@gmail.com");
+                account.setNick("007");
+                account.setPassword("123456");
+                account.setPhone("001007");
+                account.setCreatedAt(new Date());
+                account.setUpdatedAt(new Date());
+                accountReactiveRepo.save(account).subscribe();
+                System.out.println("Account imported.");
+            }
+        });
+
     }
 }
