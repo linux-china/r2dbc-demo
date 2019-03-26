e964e648f656e9826d9013953e04ea65b0d50f60
linux_china <libing.chen@gmail.com>
2019-03-26 11:20:31 -0700

    mysql script

---
+++ src/main/resources/db/migration/mysql/V1__account.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS account;
+CREATE TABLE account (
+    id         INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nick       VARCHAR(64),
+    email      VARCHAR(128),
+    phone      VARCHAR(16),
+    passwd     VARCHAR(64),
+    created_at DATETIME,
+    updated_at DATETIME
+) DEFAULT CHARSET = utf8;
