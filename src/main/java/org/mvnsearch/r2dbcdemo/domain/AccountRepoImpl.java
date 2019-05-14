8b2260a14cc5cd001cfffd304792ae18ba871d8e
linux_china <libing.chen@gmail.com>
2019-05-14 09:03:44 -0700

    api changed

--- src/main/java/org/mvnsearch/r2dbcdemo/domain/AccountRepoImpl.java
+++ src/main/java/org/mvnsearch/r2dbcdemo/domain/AccountRepoImpl.java
@@ -3,7 +3,7 @@ package org.mvnsearch.r2dbcdemo.domain;
 import io.r2dbc.client.R2dbc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.r2dbc.function.DatabaseClient;
+import org.springframework.data.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
