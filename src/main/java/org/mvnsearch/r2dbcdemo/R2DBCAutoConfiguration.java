fb71c3a9265255e79b09c5c9c750ea5dac87320a
linux_china <libing.chen@gmail.com>
2019-05-14 11:12:35 -0700

    enable postgresql

--- src/main/java/org/mvnsearch/r2dbcdemo/R2DBCAutoConfiguration.java
+++ src/main/java/org/mvnsearch/r2dbcdemo/R2DBCAutoConfiguration.java
@@ -4,9 +4,14 @@ import com.github.jasync.r2dbc.mysql.JasyncConnectionFactory;
 import com.github.jasync.sql.db.mysql.pool.MySQLConnectionFactory;
 import com.github.jasync.sql.db.mysql.util.URLParser;
 import io.r2dbc.client.R2dbc;
+import io.r2dbc.h2.H2ConnectionConfiguration;
+import io.r2dbc.h2.H2ConnectionFactory;
+import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
+import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
@@ -34,7 +39,7 @@ public class R2DBCAutoConfiguration extends AbstractR2dbcConfiguration {
         return new H2ConnectionFactory(configuration);
     }*/
 
-   /* @Bean
+    @Bean
     @Primary
     public R2dbc r2dbcPostgresql() {
         return new R2dbc(new PostgresqlConnectionFactory(postgresqlConnectionConfiguration()));
@@ -53,19 +58,19 @@ public class R2DBCAutoConfiguration extends AbstractR2dbcConfiguration {
     @Override
     public ConnectionFactory connectionFactory() {
         return new PostgresqlConnectionFactory(postgresqlConnectionConfiguration());
-    }*/
+    }
 
-    @Bean
+    /*@Bean
     public R2dbc r2dbcMySQL() {
-        String url = "mysql://root:123456@127.0.0.1:3306/test";
+        String url = "mysql://root:123456@30.56.164.234:3306/r2dbc";
         return new R2dbc(new JasyncConnectionFactory(new MySQLConnectionFactory(
                 URLParser.INSTANCE.parseOrDie(url, StandardCharsets.UTF_8))));
     }
 
     @Override
     public ConnectionFactory connectionFactory() {
-        String url = "mysql://root:123456@127.0.0.1:3306/test";
+        String url = "mysql://root:123456@30.56.164.234:3306/r2dbc";
         return new JasyncConnectionFactory(new MySQLConnectionFactory(
                 URLParser.INSTANCE.parseOrDie(url, StandardCharsets.UTF_8)));
-    }
+    }*/
 }
