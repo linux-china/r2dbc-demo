6b0802a15ce58ab9d70763dc015953cb357ba110
linux_china <libing.chen@gmail.com>
2019-12-06 23:27:12 +0800

    api adjusted

--- src/main/java/org/mvnsearch/r2dbcdemo/domain/AccountReactiveRepo.java
+++ src/main/java/org/mvnsearch/r2dbcdemo/domain/AccountReactiveRepo.java
@@ -1,6 +1,6 @@
 package org.mvnsearch.r2dbcdemo.domain;
 
-import org.springframework.data.r2dbc.repository.query.Query;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
