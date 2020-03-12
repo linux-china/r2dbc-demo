08d272252f7d594027a84e8d8979b76e88a488a8
linux_china <libing.chen@gmail.com>
2020-03-12 12:51:51 -0700

    Polishing

--- src/main/java/org/mvnsearch/r2dbcdemo/domain/Account.java
+++ src/main/java/org/mvnsearch/r2dbcdemo/domain/Account.java
@@ -4,7 +4,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
-import java.sql.Date;
 import java.time.LocalDateTime;
 
 /**
