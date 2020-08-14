1c02ae459e88501a4a6f548ed6fd9f9527b3152d
linux_china <libing.chen@gmail.com>
2020-08-13 17:06:34 -0700

    polishing

--- src/main/java/org/mvnsearch/r2dbcdemo/domain/AccountRepoImpl.java
+++ src/main/java/org/mvnsearch/r2dbcdemo/domain/AccountRepoImpl.java
@@ -1,13 +1,12 @@
 package org.mvnsearch.r2dbcdemo.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static org.springframework.data.domain.Sort.Order.desc;
+import java.time.LocalDateTime;
 
 /**
  * account repository implementation
@@ -21,16 +20,25 @@ public class AccountRepoImpl implements AccountRepo {
 
     @Override
     public Flux<Account> findAll() {
-        return databaseClient.select()
-                .from(Account.class)
-                .orderBy(Sort.by(desc("id")))
+        return databaseClient.sql("select * from account order by id desc")
                 .fetch()
-                .all();
+                .all()
+                .map(row -> {
+                    Account account = new Account();
+                    account.setId((Integer) row.get("id"));
+                    account.setNick((String) row.get("nick"));
+                    account.setEmail((String) row.get("email"));
+                    account.setPhone((String) row.get("phone"));
+                    account.setPassword((String) row.get("passwd"));
+                    account.setCreatedAt((LocalDateTime) row.get("created_at"));
+                    account.setUpdatedAt((LocalDateTime) row.get("updated_at"));
+                    return account;
+                });
     }
 
     @Override
     public Mono<Integer> updatePassword(Integer id, String newPassword) {
-        return databaseClient.execute("update account set passwd = $1 where id = $2")
+        return databaseClient.sql("update account set passwd = $1 where id = $2")
                 .bind("$1", newPassword)
                 .bind("$2", id)
                 .fetch()
